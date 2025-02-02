dc1477a4c6436da0966ec6feab608139347f84d2
bandit.studios <banditstudiosdpd@gmail.com>
2025-02-01 22:26:07 -0500

    Adjusted padding for phone view

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -18,18 +18,18 @@ export function MainLayout() {
 
   return (
     <Background>
-      <div className="flex flex-col h-full px-20">
+      <div className="flex flex-col h-full px-6 md:px-20">
         <div className="py-10">
           <h1 className="text-4xl font-bold text-white">Kareem Saygbe</h1>
         </div>
-        <div className="flex flex-1">
+        <div className="flex flex-col md:flex-row flex-1">
           {/* Left side navigation */}
-          <div className="w-1/2 p-8 flex mt-32">
+          <div className="w-full md:w-1/2 p-8 flex mt-32">
             <TextNavigation onItemSelect={handleContentSelect} />
           </div>
           
           {/* Right side content */}
-          <div className="w-1/2 p-8 flex mt-32 justify-center">
+          <div className="w-full md:w-1/2 p-8 flex mt-32 justify-center">
             {selectedContent && (
               <ContentDisplay 
                 key={selectedContent.name}
