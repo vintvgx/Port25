c26688253e0c5dde52061bf4046859408a50d65d
bandit.studios <banditstudiosdpd@gmail.com>
2025-03-26 10:17:05 -0400

    Modified Sentry trace rate + removed Sentry example page

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: "https://b100ababdb940191d4307946fb7f2702@o4506865440849920.ingest.us.sentry.io/4509039493906432",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.6,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
